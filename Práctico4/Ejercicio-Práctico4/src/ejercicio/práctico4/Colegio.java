ae91328f9f894ab2d48d3eb72aa796eeebdeb5f6
tellolaura1999 <tellolaura@sanluis.edu.ar>
2026-09-16 11:53:30 -0300

    clases

---
+++ Práctico4/Ejercicio-Práctico4/src/ejercicio/práctico4/Colegio.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio.práctico4;
+
+/**
+ *
+ * @author LAURA TELLO
+ */
+public class Colegio {
+
+    /**
+     * @param args the command line arguments
+     */
+ 
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Materia web2 = new Materia (1,"Web 2", 2);
+        Materia Matemáticas = new Materia (2, "Matemáticas", 1);
+        Materia Laboratorio1 = new Materia (3, "Laboratorio1", 1);
+        
+    Alumno alumno1 = new Alumno (1001, "López", "Martin");
+    Alumno alumno2 = new Alumno (1002, "Martínez", "Brenda"); 
+    
+    alumno1.agregarMateria(web2);
+    alumno1.agregarMateria(Matemáticas);
+    alumno1.agregarMateria(Laboratorio1);
+    
+    alumno2.agregarMateria(web2);
+    alumno2.agregarMateria(Matemáticas);
+    alumno2.agregarMateria(Laboratorio1);
+    alumno2.agregarMateria(Laboratorio1);
+    
+        System.out.println("Cantidad de materias de López: " + alumno1.cantidadMaterias());
+        System.out.println("Cantidad de materias de Martínez: " + alumno2.cantidadMaterias());
+}
+    }
+    
+   
